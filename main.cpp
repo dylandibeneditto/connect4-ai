d5e11096133ef7f7130816f8e78d2e18c96fdac2
Dylan DiBeneditto <dibenedittod@gmail.com>
2024-07-01 00:25:35 -0400

    feat: valid move check functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
+#include <algorithm>
 #include <cstdlib>
 #include <iostream>
-#include <algorithm>
 
 const int WIDTH = 7;
 const int HEIGHT = 6;
@@ -15,82 +15,102 @@ const int BGYELLOW = 103;
 
 /// @brief generic class to store any state of the board
 class Board {
-public:
-    /** @brief current move in boolean form
-     *
-     * @param 0: red's turn
-     * @param 1: yellow's turn
-     */
-    bool move = 0;
+ public:
+  /** @brief current move in boolean form
+   *
+   * @param 0: red's turn
+   * @param 1: yellow's turn
+   */
+  bool move = 0;
 
-    /** @brief board array which uses binary to display the side of a tile
-     *
-     * @attention this array relies on the `mask` array to determine whether a 0
-     * is red or an empty tile
-     *
-     * @param 0: red
-     * @param 1: yellow
-     */
-    bool side[HEIGHT][WIDTH];
+  /** @brief board array which uses binary to display the side of a tile
+   *
+   * @attention this array relies on the `mask` array to determine whether a 0
+   * is red or an empty tile
+   *
+   * @param 0: red
+   * @param 1: yellow
+   */
+  bool side[HEIGHT][WIDTH];
 
-    /** @brief board array which uses binary to display the empty state of a tile
-     *
-     * @param 0: empty
-     * @param 1: filled
-     */
-    bool mask[HEIGHT][WIDTH];
+  /** @brief board array which uses binary to display the empty state of a tile
+   *
+   * @param 0: empty
+   * @param 1: filled
+   */
+  bool mask[HEIGHT][WIDTH];
 
-    Board() {
-        for (int i = 0; i < HEIGHT; i++) {
-            for (int j = 0; j < WIDTH; j++) {
-                side[i][j] = 0;
-                mask[i][j] = 0;
-            }
-        }
+  Board() {
+    for (int i = 0; i < HEIGHT; i++) {
+      for (int j = 0; j < WIDTH; j++) {
+        side[i][j] = 0;
+        mask[i][j] = 0;
+      }
     }
+  }
 };
 
 /// @brief clears the screen based on operating system
 void clear() {
 #ifdef WINDOWS
-    std::system("cls");
+  std::system("cls");
 #else
-    std::system("clear");
+  std::system("clear");
 #endif
 }
 
 /// @brief displays the board after clearing the screen
 /// @param board
 void display(const Board& board) {
-    clear();
-    for (int i = 0; i < HEIGHT; i++) {
-        printf("%c[%dm | ", 0x1B, BLUE);
-        for (int j = 0; j < WIDTH; j++) {
-            if (board.mask[i][j] == 1) {
-                if (board.side[i][j] == 0) {
-                    printf("%c[%dmO", 0x1B, RED);
-                } else {
-                    printf("%c[%dmO", 0x1B, YELLOW);
-                }
-            } else {
-                std::cout << " ";
-            }
-            printf("%c[%dm | ", 0x1B, BLUE);
+  clear();
+  for (int i = 0; i < HEIGHT; i++) {
+    printf("%c[%dm | ", 0x1B, BLUE);
+    for (int j = 0; j < WIDTH; j++) {
+      if (board.mask[i][j] == 1) {
+        if (board.side[i][j] == 0) {
+          printf("%c[%dmO", 0x1B, RED);
+        } else {
+          printf("%c[%dmO", 0x1B, YELLOW);
         }
-        std::cout << std::endl;
-    }
-
-    std::cout << " ";
-    for (int i = 1; i <= WIDTH; i++) {
-        printf("%c[%dm  %d ", 0x1B, GRAY, i);
+      } else {
+        std::cout << " ";
+      }
+      printf("%c[%dm | ", 0x1B, BLUE);
     }
     std::cout << std::endl;
+  }
+
+  std::cout << " ";
+  for (int i = 1; i <= WIDTH; i++) {
+    printf("%c[%dm  %d ", 0x1B, GRAY, i);
+  }
+  std::cout << std::endl;
+}
+
+/** @brief returns whether column is a valid spot to move
+ * @param board: current board
+ * @param column: the column which needs to get checked
+ */
+bool isValidColumn(const Board& board, int column) {
+  return board.mask[0][column] == 0;
+}
+
+/** @brief returns whether the given position is drawn
+ * @param board: current board
+ */
+bool isDrawn(const Board& board) {
+  for (int i = 0; i < WIDTH; i++) {
+    if (isValidColumn(board, i)) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /** @brief drops the current player's piece into the given column
  *
  * @attention assumes column is a valid move
- * 
+ *
  * @param board: current board
  * @param column: the column which the piece will be dropped down (starts at
  * zero)
@@ -98,44 +118,49 @@ void display(const Board& board) {
  * @return the new instance of the board
  */
 Board makeMove(const Board& board, int column) {
-    Board newBoard;
-    newBoard.move = 1 - board.move;
+  Board newBoard;
+  newBoard.move = 1 - board.move;
 
-    // Copy the side array
-    for (int i = 0; i < HEIGHT; ++i) {
-        std::copy(board.side[i], board.side[i] + WIDTH, newBoard.side[i]);
-    }
+  // Copy the side array
+  for (int i = 0; i < HEIGHT; ++i) {
+    std::copy(board.side[i], board.side[i] + WIDTH, newBoard.side[i]);
+  }
 
-    // Copy the mask array
-    for (int i = 0; i < HEIGHT; ++i) {
-        std::copy(board.mask[i], board.mask[i] + WIDTH, newBoard.mask[i]);
-    }
+  // Copy the mask array
+  for (int i = 0; i < HEIGHT; ++i) {
+    std::copy(board.mask[i], board.mask[i] + WIDTH, newBoard.mask[i]);
+  }
 
-    for (int i = HEIGHT - 1; i >= 0; i--) {
-        if (!newBoard.mask[i][column]) {
-            newBoard.mask[i][column] = 1;
-            newBoard.side[i][column] = newBoard.move;
-            break;
-        }
+  for (int i = HEIGHT - 1; i >= 0; i--) {
+    if (!newBoard.mask[i][column]) {
+      newBoard.mask[i][column] = 1;
+      newBoard.side[i][column] = newBoard.move;
+      break;
     }
-    return newBoard;
+  }
+  return newBoard;
 }
 
 int main() {
-    Board workingBoard;
-    bool gameOver = false;
-    while (!gameOver) {
-        display(workingBoard);
-        int move;
-        printf("%c[%dm(enter your move): ", 0x1B, GRAY);
-        printf("%c[%dm", 0x1B, 97);  // reset to white for input
-        std::cin >> move;
-        move -= 1;
-        if (move >= 0 && move < WIDTH) {
-            workingBoard = makeMove(workingBoard, move);
-        } else {
-            printf("%c[%dmInvalid move. Try again.\n", 0x1B, RED);
-        }
+  Board workingBoard;
+  bool gameOver = false;
+  while (!gameOver) {
+    display(workingBoard);
+    if (isDrawn(workingBoard)) {
+      gameOver = true;
+      printf("%c[%dmGAME OVER: 1/2 - 1/2", 0x1B, 97);
+      break;
     }
-    return 0;
+    int move;
+    printf("%c[%dm(enter your move): ", 0x1B, GRAY);
+    printf("%c[%dm", 0x1B, 97);  // reset to white for input
+    std::cin >> move;
+    move -= 1;
+    if (move >= 0 && move < WIDTH) {
+      workingBoard = makeMove(workingBoard, move);
+    } else {
+      printf("%c[%dmInvalid move. Try again.\n", 0x1B, RED);
+    }
+  }
+  return 0;
 }
